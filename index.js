4cbb93e25533bdca81faabcb7d75632cd9d935a6
yGrouzelle <yan.grouzelle@edu.ece.fr>
2022-10-03 22:36:09 +0200

    exo5 lab 2

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Import a module
 const http = require('http')
+//import url module
+const url = require('url')
 
 // Define a string constant concatenating strings
 const content = '<!DOCTYPE html>' +
@@ -14,8 +16,12 @@ const content = '<!DOCTYPE html>' +
 '</html>'
 
 const serverHandle = function (req, res) {
+  // Retrieve and print the current path
+  const path = url.parse(req.url).pathname;
+  console.log(path);
+
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(content);
+  res.write(path);
   res.end();
 }
 const server = http.createServer(serverHandle);

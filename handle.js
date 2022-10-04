1c77a54bd440eae70d7280dc6c5c62aae31e41e5
yGrouzelle <yan.grouzelle@edu.ece.fr>
2022-10-04 18:06:41 +0200

    lab 2  exo8

---
+++ handle.js
@@ -0,0 +1,26 @@
+// ./handles.js
+// Necessary imports
+const url = require('url')
+const qs = require('querystring')
+module.exports = {
+    serverHandle: function (req, res){
+ // Retrieve and print the current path
+  //const path = url.parse(req.url).pathname;
+  // Retrieve and print the queryParams
+  //const queryParams = qs.parse(url.parse(req.url).query);
+  //console.log(queryParams);
+  //console.log(path);
+  const route = url.parse(req.url)
+  const path = route.pathname 
+  const params = qs.parse(route.query)
+
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  if (path === '/hello' && 'name' in params) {
+    res.write('Hello ' + params['name'])
+  } else {
+    res.write('Hello anonymous')
+  }
+  res.end();
+
+    }}
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Import a module
 const http = require('http')
 //import url module
-const url = require('url')
+//const url = require('url')
 const qs = require('querystring')
-
+const handle =require('./handle')
 // Define a string constant concatenating strings
 const content = '<!DOCTYPE html>' +
 '<html>' +
@@ -16,27 +16,9 @@ const content = '<!DOCTYPE html>' +
 '    </body>' +
 '</html>'
 
-const serverHandle = function (req, res) {
-  // Retrieve and print the current path
-  //const path = url.parse(req.url).pathname;
-  // Retrieve and print the queryParams
-  //const queryParams = qs.parse(url.parse(req.url).query);
-  //console.log(queryParams);
-  //console.log(path);
-  const route = url.parse(req.url)
-  const path = route.pathname 
-  const params = qs.parse(route.query)
 
 
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  if (path === '/hello' && 'name' in params) {
-    res.write('Hello ' + params['name'])
-  } else {
-    res.write('Hello anonymous')
-  }
-  res.end();
-}
-const server = http.createServer(serverHandle);
+const server = http.createServer(handle.serverHandle);
 server.listen(8080)
 
 //http.server

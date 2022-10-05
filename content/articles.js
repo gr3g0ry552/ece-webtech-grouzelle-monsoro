015bbd691f46d71f2e0ca69a555fc36e48589342
Gregory <gregory.monsoro@gmail.com>
2022-10-05 17:36:23 +0200

    BREAKING CHANGE: LAB3 modules plus beginning of LAB4

---
+++ content/articles.js
@@ -0,0 +1,42 @@
+let db = {
+    articles: [
+      {
+        id: '1',
+        title: 'Orange',
+        content: 'Fruit',
+        date: '04/10/2022',
+        author: 'Grégory'
+      },
+      {
+        id: '2',
+        title: 'Apple',
+        content: 'Fruit',
+        date: '04/10/2022',
+        author: 'Grégory'
+      },
+      {
+        id: '1',
+        title: 'ASUS',
+        content: 'Elctronics_brand',
+        date: '04/10/2022',
+        author: 'Grégory'
+      },
+      {
+        id: '1',
+        title: 'BMW',
+        content: 'Car_brand',
+        date: '04/10/2022',
+        author: 'Grégory'
+      },
+    ],
+    comments: [
+      {
+        id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
+        timestamp: 1664835049,
+        content: 'Content of the comment.',
+        articleId: '6ec0bd7f-11c0-43da-975e-2a8ad9ebae0b',
+        author: 'Bob McLaren'
+      },
+      // ...
+    ]
+  }
--- handle.js
+++ handle.js
@@ -2,18 +2,15 @@
 // Necessary imports
 const url = require('url')
 const qs = require('querystring')
+
 module.exports = {
-    serverHandle: function (req, res){
- // Retrieve and print the current path
-  //const path = url.parse(req.url).pathname;
-  // Retrieve and print the queryParams
-  //const queryParams = qs.parse(url.parse(req.url).query);
-  //console.log(queryParams);
-  //console.log(path);
+  serverHandle: function (req, res) {
+    
   const route = url.parse(req.url)
   const path = route.pathname 
   const params = qs.parse(route.query)
-  const content=require("./content/about.json")
+  const content = require("./content/about.json")
+  //const content = require("./content/articles")
 
   res.writeHead(200, {'Content-Type': 'text/html'});
   
@@ -29,5 +26,5 @@ module.exports = {
     res.write('code 404')
   }
   res.end();
-
-    }}
+  }
+}

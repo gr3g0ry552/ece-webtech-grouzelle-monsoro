09da2818bac4d81bc6a4788c8c6a9a46a905a4e1
gr3g0ry552 <94358887+gr3g0ry552@users.noreply.github.com>
2022-10-06 06:14:12 +0200

    FEAT! : Added port env and modify index.js

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-// Import a module
-const http = require('http')
-//import url module
-//const url = require('url')
-const qs = require('querystring')
-const handle =require('./handle')
-// Define a string constant concatenating strings
-const content = '<!DOCTYPE html>' +
-'<html>' +
-'    <head>' +
-'        <meta charset="utf-8" />' +
-'        <title>ECE AST</title>' +
-'    </head>' + 
-'    <body>' +
-'       <p>Hello World!</p>' +
-'    </body>' +
-'</html>'
+const express = require("express");
+const app = express();
+//const port = 3000;
+const port = process.env.PORT || 3000;
 
 
+//app.get()
+//app.post()
+//app.put()
+//app.delete()
 
-const server = http.createServer(handle.serverHandle);
-server.listen(8080)
+const infos = require("./content/about.json");
+//const articles = require("./content/articles");
 
-//http.server
+
+//import db from 
+
+app.get("/", (req, res) => 
+    res.send("WELCOME")); // Route handler...
+
+app.get("/api/courses", (req,res) => {
+    res.send([1,2,3]);
+})
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));

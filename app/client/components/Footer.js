f2f9c71f04cf5252af3c134792236dc301ba9d49
yan <yan.grouzelle@edu.ece.fr>
2022-11-15 18:32:21 +0100

    fix : merge problem

---
+++ app/client/components/Footer.js
@@ -0,0 +1,16 @@
+
+
+
+export default function Header(){
+  return (
+    <footer>
+      <a
+        href="https://www.adaltas.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by Adaltas
+      </a>
+    </footer>
+  )
+}

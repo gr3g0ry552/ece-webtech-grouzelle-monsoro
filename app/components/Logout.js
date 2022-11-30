0b431aeb3982a2ebe9e387a6fd04d635761bb414
yan <yan.grouzelle@edu.ece.fr>
2022-11-30 14:09:11 +0100

    fix : ajustement merge branch login-state sur main

--- app/components/Logout.js
+++ app/components/Logout.js
@@ -5,6 +5,7 @@ export default function Logout(){
 
   console.log("dans logout")
     const {login} = useContext(Context)
+    
     const onClickLogin = async (e) => {
         console.log("connection")
         const response = await fetch('https://my-json-server.typicode.com/yGrouzelle/Webtech-User/posts')

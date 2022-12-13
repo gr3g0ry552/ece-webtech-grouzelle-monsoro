35f30cc21dae9c18911758e3f8fbf793de254b8f
Gregory <gregory.monsoro@gmail.com>
2022-12-13 16:05:20 +0100

    fix: added tailwind content and plugins

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    require("tailwindcss-font-inter"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+  ],
+};

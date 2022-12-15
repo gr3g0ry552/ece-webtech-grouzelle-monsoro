2738341fb51c4afc2216d3d3f0ad221ed0d588a7
Gregory <gregory.monsoro@gmail.com>
2022-12-15 15:53:19 +0100

    fix: missing curly-brace added

--- app/Components/UserInfo.js
+++ app/Components/UserInfo.js
@@ -1,42 +1,42 @@
-import { useContext,useEffect,useState } from 'react'
-import {Context} from './UserContext'
-import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react'
+import { useContext, useEffect, useState } from "react";
+import { Context } from "./UserContext";
+import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
 import { data } from "autoprefixer";
-export default function UserInfo () {
-  const { user, logout,setUsername_contexte,username_contexte } = useContext(Context)
-  const supabase = useSupabaseClient()
+export default function UserInfo() {
+  const { user, logout, setUsername_contexte, username_contexte } =
+    useContext(Context);
+  const supabase = useSupabaseClient();
   if (user) {
-    getUsername()
+    getUsername();
   }
-  async function getUsername(){
-
+  async function getUsername() {
     let { data, error, status } = await supabase
-    .from('profiles')
-    .select(`username`)
-    .eq('id', user.id)
-    .single()
+      .from("profiles")
+      .select(`username`)
+      .eq("id", user.id)
+      .single();
     if (data) {
-    setUsername_contexte(data.username)
-    }else{
-      setUsername_contexte("non defini")
-    }  
-    
+      setUsername_contexte(data.username);
+    } else {
+      setUsername_contexte("non defini");
+    }
 
-    
-  
     return (
       <div>
         <li>
-        Username : {user ? <p>{username_contexte}</p> : <p>utilisateur non defini</p> }
+          Username :{" "}
+          {user ? <p>{username_contexte}</p> : <p>utilisateur non defini</p>}
         </li>
         <div>
-          <button onClick={()=>{ logout() }}>se deconnecter</button>
+          <button
+            onClick={() => {
+              logout();
+            }}
+          >
+            se deconnecter
+          </button>
         </div>
       </div>
-      
-      )
-
-
+    );
+  }
 }
-
-  
